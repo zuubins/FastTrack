92b2cfcf0450a3bd3a75e5e17d325d034896cfd0
zuubins <zuubins@gmail.com>
2025-03-31 13:59:53 -0400

    3/31

--- app/src/main/java/com/example/fasttrack/DBHelper.java
+++ app/src/main/java/com/example/fasttrack/DBHelper.java
@@ -3,7 +3,6 @@ package com.example.fasttrack;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
